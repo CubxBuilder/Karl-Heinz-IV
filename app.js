a78625d899d09689f32ad65e36482102c83caa69
CubeBuilder <CubeBuilder@web.de>
2026-05-29 20:30:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds, 
         GatewayIntentBits.GuildMessages, 
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
     ] 
 });
 const nachtZaehler = new Map();
