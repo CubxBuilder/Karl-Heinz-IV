cc0ddd12930ea6fb7135f14849614fe10eb0624c
CubeBuilder <CubeBuilder@web.de>
2026-08-24 19:58:03 +0200

    Add server configuration template with language support

---
+++ server-config-template.js
@@ -0,0 +1,59 @@
+module.exports = {
+    language: "de",
+    night_start: 22,
+    night_end: 6,
+    caps_percentage: 70,
+    punishment_caps: "none",
+    punishment_night: "none",
+    punishment_night_trigger_count: 5,
+    phrases: {
+        de: {
+            night: [
+                "Es ist nach 22 Uhr! Schleichts euch ins Bett!",
+                "Ab ins Körbchen, es ist Nachtruhe!",
+                "Wer jetzt noch schreibt, kriegt eine mit'm Pantoffel!",
+                "Ruhe im Karton hier, Karl Heinz will schlafen!",
+                "Hush jetzt, die Nachtwache duldet kein Geplapper!"
+            ],
+            caps: [
+                "Schrei mi ned so o! Mach die Feststelltaste aus!",
+                "Warum brüllst du so? Ich bin nicht taub!",
+                "Brüll woanders rum, Kollege!",
+                "Tastatur kaputt oder warum schreibst du so groß?!",
+                "Leiser schreiben, bitte!"
+            ]
+        },
+        en: {
+            night: [
+                "It's past 10 PM! Go to sleep!",
+                "Bedtime! Keep it quiet!",
+                "Quiet down, Karl Heinz is trying to sleep!",
+                "Night mode is active, shhh!",
+                "No talking during quiet hours!"
+            ],
+            caps: [
+                "Stop yelling! Turn off Caps Lock!",
+                "Why are you shouting? I'm not deaf!",
+                "Calm down with the capital letters!",
+                "Is your keyboard broken?",
+                "Keep your voice down here!"
+            ]
+        },
+        bar: {
+            night: [
+                "Es is nacha zehne! Ab ins Bett etzat!",
+                "Schleichs eich in d'Heia, Nachtruah is!",
+                "Wer iazt no plärrt, kriagt a Watschn!",
+                "Ruahgstellt werd, da Karl Heinz mog pennen!",
+                "Gschpariats eich d'Sprüch für moagn auf!"
+            ],
+            caps: [
+                "Plärr ned so dumm rum da!",
+                "I bin ned daub, mach d'Feststelltastn aus!",
+                "Wieso schraist'n so, herst?",
+                "Tastatua hi oda wos is los?",
+                "Ganz gschmeidig bleibm, ned so rumbrüin!"
+            ]
+        }
+    }
+};
